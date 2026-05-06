e2d95584ae94baf8e3e8ea3be174a3fb0bd35eb8
cekharchandra-devops <cekharchandra.devops@gmail.com>
2026-05-06 07:05:54 +0530

    terraform script added for vars and counts

---
+++ 03-vars/main.tf
@@ -0,0 +1,66 @@
+variable "location" {
+    type        = string
+    description = "The Azure region to deploy resources in"
+    default     = "Denmark East"
+}
+
+variable "resource_group_name" {
+    type        = string
+    description = "The name of the resource group to create"
+    default     = "dnmrk-est-rg"
+}       
+
+variable "secure_boot_enabled" {
+  type = bool
+  description = "Enable or disable secure boot for the virtual machine"
+  default = true
+}
+
+variable "list_of_tags" {
+  type = map(string)
+  description = "A map of tags to apply to the resources"
+  default = {
+    environment = "staging"
+    project     = "roboshop"
+  }
+}
+
+resource "azurerm_public_ip" "main" {
+  name                = "test-public-ip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Dynamic"
+  
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "test-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "testipconfig"
+    subnet_id                     = "/subscriptions/e95ed2ec-55a5-49ac-a41d-51cb0ac50b67/resourceGroups/dnmrk-est-rg/providers/Microsoft.Network/virtualNetworks/custom-vm-vnet/subnets/default"
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.main.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "test-vm" {
+  name                = "test-vm"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = "Standard_B2s" # Ensure size supports Trusted Launch
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  network_interface_ids = [azurerm_network_interface.main.id]
+  admin_username       = "azureuser"
+  admin_password       = "P@ssw0rd123!"
+  secure_boot_enabled  = var.secure_boot_enabled
+  vtpm_enabled = true
+  tags                 = var.list_of_tags 
+  disable_password_authentication = false
+  source_image_id = "/subscriptions/e95ed2ec-55a5-49ac-a41d-51cb0ac50b67/resourceGroups/dnmrk-est-rg/providers/Microsoft.Compute/galleries/rhel10/images/1.0.0"
+}
---
+++ 04-count/main.tf
@@ -0,0 +1,22 @@
+resource "null_resource" "main" {
+  count = 10
+}
+
+variable "component" {
+  type = list(string)
+  default = [ "frontend", "backend", "database" ]
+}
+
+resource "azurerm_network_interface" "main" {
+    count = 3
+    name                = "${var.component[count.index]}-${count.index}"
+    location            = "Denmark East"
+    resource_group_name = "dnmrk-est-rg"
+    
+    ip_configuration {
+        name                          = "${var.component[count.index]}-ipconfig"
+        subnet_id                     = "/subscriptions/e95ed2ec-55a5-49ac-a41d-51cb0ac50b67/resourceGroups/dnmrk-est-rg/providers/Microsoft.Network/virtualNetworks/custom-vm-vnet/subnets/default"
+        private_ip_address_allocation = "Dynamic"
+        # public_ip_address_id = azurerm_public_ip.main.id
+    }
+}
